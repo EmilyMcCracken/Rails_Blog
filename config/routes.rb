7e2ba9d29ca73b585487a0b4a00bb50b77764262
Emily McCracken <emilymcc803@gmail.com>
2016-01-09 13:42:43 -0500

    added header to layout view and app css, nav not included

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'users#new'
 
   get 'login' => 'sessions#new', as: :login
   post 'login' => 'sessions#create'
