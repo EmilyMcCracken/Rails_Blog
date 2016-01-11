f2ddef211b64a50fa9737eb774f967b99d50163a
Emily McCracken <emilymcc803@gmail.com>
2016-01-11 17:38:46 -0500

    edit profile working, all links working

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,7 +9,7 @@ class ProfilesController < ApplicationController
     @profile.save 
     @user.profile = @profile
 
-    redirect_to root_path
+    redirect_to @user
   end
 
   def show
@@ -23,18 +23,10 @@ class ProfilesController < ApplicationController
   def update
     @profile = Profile.find(params[:id])
     @user = current_user
-    if @profile != nil
-      if current_user.id == @profile.user_id
+    current_user.id == @profile.user_id
          @profile.update(profile_params)
          redirect_to user_path @user
          flash[:notice] = "Profile Updated!"
-      else
-        redirect_to user_path @user
-        flash[:alert] = "Nice Try Buddy. Not your profile."
-      end
-    else
-        redirect_to new_profile_path
-    end  
   end
 
     private
